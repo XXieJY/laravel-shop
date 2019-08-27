10ed0d80abd8ec24e8a489b867bc3698b4f10086
wangguoqing <965490571@qq.com>
2019-08-27 21:33:27 +0800

    add

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,12 +1,21 @@
 <?php
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
+use App\User;
 class AuthController extends Controller{
 
      public function wechatStore(){
 
           $wechatUser = session('wechat.oauth_user.default');
+          $user = User::where('weixin_openid',$wechatUser->id)->first();
+          if(!$user){
+              $user = User::create([
+                  'nickname' => $wechatUser->name,
+                  'weixin_openid' => $wechatUser->id,
+                  'image_head' => $wechatUser->avatar
+              ]);
+          }
 
-          dd($wechatUser);
+          return "通过";
      }
 }
--- app/User.php
+++ app/User.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    protected $table = "sys_user";
+    protected $fillable = [
+        'nickname','weixin_openid','image_head'
+    ];
 }
