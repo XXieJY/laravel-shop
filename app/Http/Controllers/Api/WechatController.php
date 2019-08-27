645d88e9c923cda89cae5db7370e7b58f787c258
wangguoqing <965490571@qq.com>
2019-08-27 13:31:15 +0800

    modify

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -3,8 +3,32 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 class WechatController extends Controller{
 
+    const TOKEN = "wangguoqing";
     public function index(){
 
-       echo request()->query('token');
+        $echoStr = request()->query("echostr");
+
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = request()->query("signature");
+        $timestamp = request()->query("timestamp");
+        $nonce = request()->query("nonce");
+        $token = self::TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
