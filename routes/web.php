d09998de7bc1466cd8b69d71d69b4aa536a4340b
wangguoqing <965490571@qq.com>
2019-08-27 16:39:10 +0800

    add

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::prefix('api/')->group(function (){
 
     Route::get('wechat/index','Api\WechatController@index');
-    Route::get('auth/wechatStore','Api\AuthController@wechatStore');
+    Route::get('auth/wechatStore','Api\AuthController@wechatStore')->middleware("wechat.oauth");
 });
 Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
     Route::get('/user', function () {
