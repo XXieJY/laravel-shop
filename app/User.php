dcff68d10be80f8ec8ffd6db79ce7ccbd2a87dd2
wangguoqing <965490571@qq.com>
2019-09-01 20:23:11 +0800

    add

--- app/User.php
+++ app/User.php
@@ -3,13 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model
+class User extends Model implements JWTSubject
 {
     protected $table = "user";
     protected $fillable = [
         'nickname','weixin_openid','image_head'
     ];
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        // TODO: Implement getJWTIdentifier() method.
+    }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        // TODO: Implement getJWTCustomClaims() method.
+    }
 }
